e224b23cc392039b8717fcd6fe15705dfb6be63f
Dmytro Ch <64646103+TheDeiw@users.noreply.github.com>
2024-01-28 01:03:01 +0200

    Working buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-window.onload = function reveal() {
+/*window.onload = function reveal() {
     var reveal = document.querySelector(".reveal");
     reveal.classList.add("active");
+}*/
+//reveals[i].classList.remove("active");
+function reveal(name){
+    var reveal = document.querySelector(name);
+    reveal.classList.add("active");
 }
+
+function hide(name){
+    var reveal = document.querySelector(name);
+    reveal.classList.remove("active");
+}
+
+window.addEventListener("load", function() {
+    reveal(".registration-field");
+});
