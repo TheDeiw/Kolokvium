65a82665932fa1d4967aad2c34172b2cdc3a4826
Dmytro Ch <64646103+TheDeiw@users.noreply.github.com>
2024-01-30 16:08:50 +0200

    Working with animations

--- js/script.js
+++ js/script.js
@@ -5,12 +5,20 @@
 //reveals[i].classList.remove("active");
 function reveal(name){
     var reveal = document.querySelector(name);
-    reveal.classList.add("active");
+    reveal.classList.remove("hidden");
+    setTimeout(function () {
+        reveal.style.display = "block";
+        reveal.classList.add("active");
+    }, 1000);
 }
 
 function hide(name){
     var reveal = document.querySelector(name);
     reveal.classList.remove("active");
+    setTimeout(function () {
+        reveal.style.display = "none";
+        reveal.classList.add("hidden");
+    }, 1000);
 }
 
 window.addEventListener("load", function() {
--- js/sphereMovement.js
+++ js/sphereMovement.js
@@ -29,10 +29,11 @@
     //animateDiv('#sphere3');
 });*/
 
+/*
 const root = document.querySelector(":root"); // we first get the root element
 for(let i = 0; i < 10; i++) {
   root.style.setProperty(`--rando${i}`, `${Math.floor(Math.random() * 200) + 1}px`);
-}
+}*/
 
 
 /*
